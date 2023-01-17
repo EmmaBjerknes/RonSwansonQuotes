5b5208dee190264a634337d988d2b7d5362b1496
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-01-17 15:29:43 +0100

    Added nav buttons, next working ugly atm

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,40 @@ const quotesCard = document.querySelector('#quotesDiv') as HTMLDivElement;
 const quotesParagraf = document.createElement ('p');
 quotesParagraf.innerHTML = "Give me a sec to get a nice epic quote for you...";
 quotesCard.append(quotesParagraf);
+const likeBtn = document.querySelector('#likeBtn') as HTMLButtonElement;
+const nxtBtn = document.querySelector('#nextBtn') as HTMLButtonElement;
 
-const url = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+let nrClicked: number = 0;
+
+const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes/15";
 
 const getUrl = fetch(url)
     .then(response =>{
         return response.json();
     }).then(quoteArr =>{
         quotesParagraf.innerHTML= "";
+        showQuote(quoteArr, nrClicked) 
+    });
 
-        showQuote(quoteArr) 
-    })
 
-function showQuote(quoteArr: string):void{
-    quotesParagraf.innerHTML = quoteArr;
+function showQuote(quoteArr:any, nrClicked:number):void{
+    console.log(typeof quoteArr);
+    quotesParagraf.innerHTML = quoteArr[nrClicked];
     quotesCard.append(quotesParagraf); 
+
+    nxtBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        quotesParagraf.innerHTML = "";
+        nrClicked++; 
+        if (nrClicked>=15){
+            nrClicked = 0;
+        }
+        quotesParagraf.innerHTML = quoteArr[nrClicked];
+    })
 }
+
+
+likeBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('Love this!');
+})
