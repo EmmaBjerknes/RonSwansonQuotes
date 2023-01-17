9788b21f1705d271de098fc8bee077c255288340
EmmaBjerknes <121795531+EmmaBjerknes@users.noreply.github.com>
2023-01-17 13:43:56 +0100

    Delete dist directory

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const url = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
-const getUrl = fetch(url)
-    .then(response => {
-    return response.json();
-}).then(ronObj => {
-    console.log(ronObj);
-});
