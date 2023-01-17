50594f02aac54cc8e2a6fd194c27e46eff3198a8
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-01-17 14:24:28 +0100

    Basic code for displaying quotes on webpage

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
+const quotesCard = document.querySelector('#quotesDiv') as HTMLDivElement;
+const quotesParagraf = document.createElement ('p');
+quotesParagraf.innerHTML = "Give me a sec to get a nice epic quote for you...";
+quotesCard.append(quotesParagraf);
+
 const url = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
 const getUrl = fetch(url)
     .then(response =>{
         return response.json();
-    }).then(ronObj =>{
-      console.log(ronObj);  
+    }).then(quoteArr =>{
+        quotesParagraf.innerHTML= "";
+
+        showQuote(quoteArr) 
     })
 
+function showQuote(quoteArr: string):void{
+    quotesParagraf.innerHTML = quoteArr;
+    quotesCard.append(quotesParagraf); 
+}
